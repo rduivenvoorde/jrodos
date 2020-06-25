603874bbe51f5a50679d57b38b4dc3589439d3ca
Richard Duivenvoorde <richard@duif.net>
2020-06-25 12:36:34 +0200

    Just to be sure we do maximize page size

--- jrodos_settings.py
+++ jrodos_settings.py
@@ -33,7 +33,7 @@ class JRodosSettings(SettingManager):
         # measurements_wfs_url
         self.add_setting(String('measurements_wfs_url', Scope.Global,
                                 'http://geoserver.prd.cal-net.nl/geoserver/radiation.measurements/ows?'))
-        self.add_setting(Integer('measurements_wfs_page_size', Scope.Global, 10000))
+        self.add_setting(Integer('measurements_wfs_page_size', Scope.Global, 25000))
         # measurements_soap_utils_url
         self.add_setting(String('measurements_soap_utils_url', Scope.Global,
                                 'http://geoserver.prd.cal-net.nl/calnet-measurements-ws/utilService'))

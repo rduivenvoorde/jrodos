ee62d714cfb2de1393544ea0a37d4c924e2b4130
Richard Duivenvoorde <richard@duif.net>
2022-04-26 10:42:48 +0200

    Add UTC in grafiek

--- ui/jrodos_graph_widget.py
+++ ui/jrodos_graph_widget.py
@@ -63,7 +63,7 @@ class JRodosGraphWidget(QDockWidget, FORM_CLASS):
 
         pw = pg.PlotWidget(axisItems={'bottom': x_axis, 'left': y_axis},
                            enableMenu=False,
-                           title="y: Values  -   x: Time")
+                           title="y: Values  -   x: Time (UTC)")
 
         pw.show()
         self.graph = pw
